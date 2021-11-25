34fb42170a067cff984a66e38a947ff6dd64a5fa
Mizzoo <87053514+Mizzuu@users.noreply.github.com>
2021-11-26 08:28:48 +0900

    first set up

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
+get_header();
+
 echo "Hey, this is MIZUHO";
+
+if (have_posts()) :
+    while (have_posts()) :
+        the_post();
+        the_title();
+        the_post_thumbnail();
+        the_content();
+        the_category();
+    endwhile;
+    else :
+        echo "No posts";
+    endif;
+
+get_footer();
